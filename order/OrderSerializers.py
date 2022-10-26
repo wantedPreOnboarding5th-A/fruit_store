5c5608ffb43424a40f5c9a33d25af4a14390574b
JunH <101803254+Blaze-F@users.noreply.github.com>
2022-10-26 21:45:31 +0900

    [Add] order 및 하위 모델 단일 개체 Seralizers 작성
    컬렉션으로 Serialize하는 경우는 아직 미작성

---
+++ order/OrderSerializers.py
@@ -0,0 +1,81 @@
+from rest_framework import serializers
+
+from order.models import Order, OrderDeilivery, OrderPayment, OrderTransaction, ProductOut
+
+"""
+직렬화를 담당
+Order, OrderPayment, OrderDelivery, ProductOut
+
+Detail page에 들어갈 정보 -> 단일만 조회  = OrderTransaction, OrderPayments
+
+조회시 일부 컬럼만 리스트로 직렬화 -> Order, #To-do
+"""
+# Question : Serialize에서 DB의 엔티티를 조회시 하위 엔티티들은 어떻게 처리되는가.
+# Question : Serialize 할때 필드명을 명시하는게 좋은가
+# Question : Order에는 Total fee, Total Price (장바구니 합계를 저장)
+# Question : cascade
+
+"""
+단일 Order
+"""
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = "__all__"
+
+
+"""
+Order List
+"""
+
+
+class OrderListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = "__all__"  # To-do 리스트로 조회시 필드명 정리
+
+
+"""
+출고 테이블
+"""
+
+
+class ProductOutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductOut
+        fields = "__all__"
+
+
+"""
+지불정보
+"""
+
+
+class OrderPaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderPayment
+        fields = "__all__"
+
+
+"""
+배송정보
+"""
+
+
+class OrderDeliverySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderDeilivery
+        fields = "__all__"
+
+
+"""
+결제정보
+"""
+
+
+class OrderTransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderTransaction
+        fields = "__all__"
