f780d90aca01a023a7efdc1be9bb4a1c21bf4b58
ehdrb92 <kcj920407@gmail.com>
2022-10-27 14:42:42 +0900

    docs: 환경변수 저장 방식 수정  Please enter the commit message for your changes. Lines starting

--- provider/storage_provider.py
+++ provider/storage_provider.py
@@ -1,11 +1,3 @@
 from django.conf import settings
 
 # S3 이미지 업로드 관련 클래스 구현
-
-class AmazonS3Provider:
-    def __init__(self) -> None:
-        self.default_file_storage = 'storages.backends.s3boto3.S3Boto3Storage'
-        self.aws_access_key_id = 'AKIA2Z7E4H24OEU34B7H'
-        self.aws_secret_access_key = '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'
-        self.aws_storage_bucket_name = 'fruit-store'
-        self.aws_querystring_auth = False
