8fe2f4ba310356b6d59e5054ca6b5890625db71f
ehdrb92 <kcj920407@gmail.com>
2022-10-28 01:34:30 +0900

    feat: product 관련 url 추가 및 기타 수정

---
+++ product/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from product.controller import create, delete, get_detail, get_list
+
+urlpatterns = [
+    path("register/", create),
+    path("list/", get_list),
+    path("detail/", get_detail),
+    path("delete/", delete),
+]
