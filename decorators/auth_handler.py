3bb64e33584e1e04340866a1e4b0958157fe7d49
yomapi <yomapi.new@gmail.com>
2022-10-28 20:03:38 +0900

    feat: auth provider APIVIEW 호환

--- decorators/auth_handler.py
+++ decorators/auth_handler.py
@@ -1,12 +1,14 @@
 from functools import wraps
 from provider.auth_provider import auth_provider
 from exceptions import NotAuthorizedError
+from rest_framework.views import APIView
 
 
 def must_be_user():
     def decorator(api_func):
         @wraps(api_func)
         def _wrapped_view(request, *args, **kwargs):
+            request = request.request if isinstance(request, APIView) else request
             auth_token = auth_provider.get_token_from_request(request)
             if auth_token == None:
                 raise NotAuthorizedError
@@ -23,6 +25,7 @@ def must_be_admin():
     def decorator(api_func):
         @wraps(api_func)
         def _wrapped_view(request, *args, **kwargs):
+            request = request.request if isinstance(request, APIView) else request
             auth_token = auth_provider.get_token_from_request(request)
             if auth_token == None:
                 raise NotAuthorizedError
